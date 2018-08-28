10b0d516248706360773bb936d31c225bb4368a2
Jeongdasilver <alal8272@gmail.com>
2018-08-28 22:51:46 +0900

    init

---
+++ Weekend-Farm-Admin/app/src/main/java/com/chat/jumpup/weekend_farm_admin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.chat.jumpup.weekend_farm_admin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
