bcbb80c35d10c7287912ede220c794bac4af6ed2
Sungyong Son <space010127@gmail.com>
2018-08-31 21:26:00 +0900

    fix

--- Weekend-Farm-User/app/src/main/java/com/farm/weekend/gram/weekend_farm_user/fragment/SearchFragment.java
+++ Weekend-Farm-User/app/src/main/java/com/farm/weekend/gram/weekend_farm_user/fragment/SearchFragment.java
@@ -5,10 +5,32 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.NumberPicker;
 
 import com.farm.weekend.gram.weekend_farm_user.R;
 
 public class SearchFragment extends Fragment {
+
+    String[] doData = new String[]{
+            "서울특별시",
+            "부산광역시",
+            "대구광역시",
+            "인천광역시",
+            "광주광역시",
+            "대전광역시",
+            "울산광역시",
+            "세종특별자치시",
+            "경기도",
+            "강원도",
+            "충청북도",
+            "충청남도",
+            "전라북도",
+            "전라남도",
+            "경상북도",
+            "경상남도",
+            "제주도"
+    };
+    NumberPicker numberPicker;
 
     public static SearchFragment create() {
         SearchFragment fragment = new SearchFragment();
@@ -21,9 +43,12 @@ public class SearchFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.search_fragment, container, false);
+        numberPicker = rootView.findViewById(R.id.do_picker);
+        numberPicker.setMinValue(0);
+        numberPicker.setMaxValue(16);
+        numberPicker.setDisplayedValues(doData);
         return rootView;
     }
 }
