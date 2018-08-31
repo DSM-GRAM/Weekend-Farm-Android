529f8869e27ea0d574456b72d7c2fc02b1d397a2
Sungyong Son <space010127@gmail.com>
2018-09-01 00:40:34 +0900

    [Update] FarmInfo

--- Weekend-Farm-User/app/src/main/java/com/farm/weekend/gram/weekend_farm_user/activity/FarmInfoActivity.java
+++ Weekend-Farm-User/app/src/main/java/com/farm/weekend/gram/weekend_farm_user/activity/FarmInfoActivity.java
@@ -1,8 +1,6 @@
 package com.farm.weekend.gram.weekend_farm_user.activity;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.farm.weekend.gram.weekend_farm_user.R;
@@ -10,8 +8,8 @@ import com.farm.weekend.gram.weekend_farm_user.R;
 public class FarmInfoActivity extends AppCompatActivity {
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_farm_info);
     }
 }
