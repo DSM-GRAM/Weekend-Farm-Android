31eb58da22ec819c9d3013c67511b3baeb94f8e0
Sungyong Son <space010127@gmail.com>
2018-08-31 19:10:22 +0900

    [Update] ShopRecycler

--- Weekend-Farm-User/app/src/main/java/com/farm/weekend/gram/weekend_farm_user/adapter/ShopItemAdapter.java
+++ Weekend-Farm-User/app/src/main/java/com/farm/weekend/gram/weekend_farm_user/adapter/ShopItemAdapter.java
@@ -1,11 +1,14 @@
 package com.farm.weekend.gram.weekend_farm_user.adapter;
 
+import android.annotation.SuppressLint;
+
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,16 +21,16 @@ import com.farm.weekend.gram.weekend_farm_user.fragment.BuyDialogFragment;
 import com.farm.weekend.gram.weekend_farm_user.model.MyFarmItemModel;
 import com.farm.weekend.gram.weekend_farm_user.model.ShopItemModel;
 
-import java.io.FileDescriptor;
-import java.io.PrintWriter;
 import java.util.List;
 
 public class ShopItemAdapter extends RecyclerView.Adapter<ShopItemAdapter.ViewHolder>{
 
     private final List<ShopItemModel> items;
+    private final Context context;
 
-    public ShopItemAdapter(List<ShopItemModel> items) {
+    public ShopItemAdapter(List<ShopItemModel> items, Context context) {
         this.items = items;
+        this.context = context;
     }
 
     @NonNull
@@ -38,16 +41,19 @@ public class ShopItemAdapter extends RecyclerView.Adapter<ShopItemAdapter.ViewHo
         return new ViewHolder(v);
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         ShopItemModel item = items.get(position);
-        holder.item_text.setText(item.buyItemName + " " + item.buyItemNum + "개");
-        holder.item_money.setText(item.buyItemMoney + "원");
+        holder.item_text.setText(item.getBuyItemName() + " " + item.getBuyItemNum() + "개");
+        holder.item_money.setText(item.getBuyItemMoney() + "원");
         holder.CallDialog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                BuyDialogFragment dialog = new BuyDialogFragment(this);
-                dialog.show();
+                FragmentManager fragmentManager = ((AppCompatActivity)context).getSupportFragmentManager();
+                BuyDialogFragment dialog = new BuyDialogFragment();
+                dialog.setCancelable(false);
+                dialog.show(fragmentManager, "BUY");
             }
         });
     }
@@ -61,12 +67,11 @@ public class ShopItemAdapter extends RecyclerView.Adapter<ShopItemAdapter.ViewHo
         Button CallDialog;
         TextView item_text;
         TextView item_money;
-                ViewHolder(View viewItems) {
-                    super(viewItems);
+        ViewHolder(View viewItems) {
+            super(viewItems);
             CallDialog = (Button) viewItems.findViewById(R.id.buying_button);
             item_money = (TextView) viewItems.findViewById(R.id.item_price);
-            item_text = (TextView) viewItems.findViewById(R.id.item_text);
-
+            item_text = (TextView) viewItems.findViewById(R.id.item_name);
         }
     }
 }
--- Weekend-Farm-User/app/src/main/java/com/farm/weekend/gram/weekend_farm_user/fragment/ShopFragment.java
+++ Weekend-Farm-User/app/src/main/java/com/farm/weekend/gram/weekend_farm_user/fragment/ShopFragment.java
@@ -2,15 +2,27 @@ package com.farm.weekend.gram.weekend_farm_user.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.farm.weekend.gram.weekend_farm_user.R;
+import com.farm.weekend.gram.weekend_farm_user.adapter.ShopItemAdapter;
+import com.farm.weekend.gram.weekend_farm_user.model.ShopItemModel;
+
+import java.util.ArrayList;
 
 public class ShopFragment extends Fragment {
 
+    ArrayList<ShopItemModel> shopItemModels = new ArrayList<>();
+    ShopItemAdapter shopItemAdapter;
+    RecyclerView recyclerView;
+    LinearLayoutManager linearLayoutManager;
 
     public static Fragment create() {
         ShopFragment fragment = new ShopFragment();
@@ -20,12 +32,23 @@ public class ShopFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.shop_fragment, container, false);
+        recyclerView = rootView.findViewById(R.id.shop_recycler);
+
+        shopItemModels.add(new ShopItemModel("김윤재", 100, 100000));
+        shopItemAdapter = new ShopItemAdapter(shopItemModels, getContext());
+        linearLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setAdapter(shopItemAdapter);
+        recyclerView.setHasFixedSize(false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
         return rootView;
     }
 }
