e95d1dbffb74a42fad3a42039a2a382e22fc308e
Sungyong Son <space010127@gmail.com>
2018-09-01 03:15:18 +0900

    [Update] Search Farm

--- Weekend-Farm-User/app/src/main/java/com/farm/weekend/gram/weekend_farm_user/connect/API.java
+++ Weekend-Farm-User/app/src/main/java/com/farm/weekend/gram/weekend_farm_user/connect/API.java
@@ -2,12 +2,23 @@ package com.farm.weekend.gram.weekend_farm_user.connect;
 
 import com.google.gson.JsonArray;
 
+import org.json.JSONArray;
+
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface API {
     @GET("users/{user}/repos")
     Call<JsonArray> getUserRepositories(@Path("user") String userName);
 
+    @Headers({"Content-Type: application/json;charset=UTF-8"})
+    @GET("/user/search")
+    Call<JsonArray> getSearchResult(@Header("Authorization") String Authorization, @Query("donum") int donum);
 }
--- Weekend-Farm-User/app/src/main/java/com/farm/weekend/gram/weekend_farm_user/connect/Connector.java
+++ Weekend-Farm-User/app/src/main/java/com/farm/weekend/gram/weekend_farm_user/connect/Connector.java
@@ -9,7 +9,7 @@ public final class Connector {
 
     static {
         //String TEST_URL = "https://api.github.com/";
-        String URL = "URL HERE";
+        String URL = "http://13.124.114.186/";
         Retrofit retrofit = new Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(new OkHttpClient.Builder().build())
