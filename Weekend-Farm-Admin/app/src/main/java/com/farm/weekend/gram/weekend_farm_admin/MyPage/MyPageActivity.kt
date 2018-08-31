3ec1e9d8be9dd0081922f5d47b45bfcdadcb0a43
Jeongdasilver <alal8272@gmail.com>
2018-09-01 01:41:02 +0900

    add radiobutton setcurrentitem

--- Weekend-Farm-Admin/app/src/main/java/com/farm/weekend/gram/weekend_farm_admin/MyPage/MyPageActivity.kt
+++ Weekend-Farm-Admin/app/src/main/java/com/farm/weekend/gram/weekend_farm_admin/MyPage/MyPageActivity.kt
@@ -7,6 +7,7 @@ import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
 import android.support.v4.view.ViewPager
 import com.farm.weekend.gram.weekend_farm_admin.R
+import kotlinx.android.synthetic.main.activity_my_page.*
 
 class MyPageActivity : AppCompatActivity() {
     private lateinit var viewPager: ViewPager
@@ -22,6 +23,14 @@ class MyPageActivity : AppCompatActivity() {
         viewPager.adapter = pagerAdapter
         viewPager.setCurrentItem(0)
 
+
+        radiobtn_my_farm.setOnClickListener {
+            viewPager.setCurrentItem(0)
+        }
+        radiobtn_my_store.setOnClickListener {
+            viewPager.setCurrentItem(1)
+        }
+
     }
 
     class PagerAdapter(fm: FragmentManager?) : FragmentStatePagerAdapter(fm) {
@@ -30,7 +39,7 @@ class MyPageActivity : AppCompatActivity() {
         }
 
         override fun getItem(position: Int): Fragment? {
-            when(position) {
+            when (position) {
                 0 -> return MyPageFarmInformFragment()
                 1 -> return MyPageStoreInformFragment()
                 else -> return null
