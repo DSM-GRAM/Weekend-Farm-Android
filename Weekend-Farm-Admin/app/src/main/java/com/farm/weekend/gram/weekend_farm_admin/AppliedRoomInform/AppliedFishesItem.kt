a641bd4aeb19eec6f6a4c3e357558fab71ac752c
Jeongdasilver <alal8272@gmail.com>
2018-08-31 17:38:48 +0900

    add AppliedRoomInformActivity

---
+++ Weekend-Farm-Admin/app/src/main/java/com/farm/weekend/gram/weekend_farm_admin/AppliedRoomInform/AppliedFishesItem.kt
@@ -0,0 +1,10 @@
+package com.farm.weekend.gram.weekend_farm_admin.AppliedRoomInform
+
+class AppliedFishesItem {
+    var fishName: String = ""
+    var fishNumber: String = ""
+    constructor(fishName: String, fishNumber: String) {
+        this.fishName = fishName
+        this.fishNumber = fishNumber
+    }
+}
