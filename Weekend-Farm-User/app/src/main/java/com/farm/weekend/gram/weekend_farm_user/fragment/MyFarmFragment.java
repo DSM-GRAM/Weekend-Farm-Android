3f7c0b0e4ce54be17a06ea0eee3c20f5835cdd41
jinusong <sugwer7464@gmail.com>
2018-08-31 17:57:57 +0900

    RecyclerView View

--- Weekend-Farm-User/app/src/main/java/com/farm/weekend/gram/weekend_farm_user/fragment/MyFarmFragment.java
+++ Weekend-Farm-User/app/src/main/java/com/farm/weekend/gram/weekend_farm_user/fragment/MyFarmFragment.java
@@ -2,13 +2,28 @@ package com.farm.weekend.gram.weekend_farm_user.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.farm.weekend.gram.weekend_farm_user.R;
+import com.farm.weekend.gram.weekend_farm_user.adapter.MyFarmFishAdapter;
+import com.farm.weekend.gram.weekend_farm_user.model.MyFarmFishModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyFarmFragment extends Fragment {
+
+    MyFarmFishAdapter mAdapter;
+    RecyclerView mRecyclerView;
+    LinearLayoutManager linearLayoutManager;
+
+    List<MyFarmFishModel> FishModel;
+
 
     public static Fragment create() {
         MyFarmFragment fragment = new MyFarmFragment();
@@ -18,12 +33,27 @@ public class MyFarmFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        addData();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.my_farm_fragment, container, false);
+        FishModel = new ArrayList<>();
+        FishModel.add(new MyFarmFishModel("김윤재", 1000));
+        mAdapter = new MyFarmFishAdapter(FishModel);
+        mRecyclerView = rootView.findViewById(R.id.fish_recycler);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setAdapter(mAdapter);
+        linearLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
         return rootView;
+    }
+
+    public void addData(){
+
     }
 }
